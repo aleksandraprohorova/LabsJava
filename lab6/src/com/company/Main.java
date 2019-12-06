05b3a8149445c4f849102332a5c2b08b4f495e70
Alexandra <adlovlac@gmail.com>
2019-12-06 07:25:34 +0300

    Убраны константы с именами входных файлов

--- lab6/src/com/company/Main.java
+++ lab6/src/com/company/Main.java
@@ -2,11 +2,17 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<Integer, Account> accounts = AccountsParser.parse("accounts");
-        ArrayList<Transfer> transfers = TransferParser.parse("transfers", accounts);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите путь до файла с данными про счета:");
+        String pathForAccounts = in.next();
+        HashMap<Integer, Account> accounts = AccountsParser.parse(pathForAccounts);
+        System.out.println("Введите путь до файла с данными про переводы:");
+        String pathForTransfers = in.next();
+        ArrayList<Transfer> transfers = TransferParser.parse(pathForTransfers, accounts);
         if (transfers == null)
         {
             System.out.println("Ошибка при обработке входных данных.");
